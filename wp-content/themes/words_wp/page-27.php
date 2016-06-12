0d6f294a63a65e2eae506d292203c4725f881684
Jack Lovell <jacklovell@gmail.com>
2016-06-11 17:23:22 -0700

    clean up code

--- wp-content/themes/words_wp/page-27.php
+++ wp-content/themes/words_wp/page-27.php
@@ -18,23 +18,6 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<?php
-
-
-
-
-			// JL
-			/*
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			*/
 			$args = array( 'post_type' => 'movies', 'posts_per_page' => 10 );
 			$loop = new WP_Query( $args );
 			while ( $loop->have_posts() ) : $loop->the_post();
@@ -43,9 +26,6 @@ get_header(); ?>
 			    the_content();
 			    echo '</div>';
 			endwhile;
-
-
-
 			?>
 
 		</main><!-- #main -->
