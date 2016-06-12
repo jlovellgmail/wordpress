5d121a495f0f8b19889defc0e9eb96786997a519
Jack Lovell <jacklovell@gmail.com>
2016-06-11 17:40:22 -0700

    add style, hide all wordpress default template stuff, and get the content without paragraph tags

--- wp-content/themes/words_wp/page-27.php
+++ wp-content/themes/words_wp/page-27.php
@@ -21,9 +21,14 @@ get_header(); ?>
 			$args = array( 'post_type' => 'movies', 'posts_per_page' => 10 );
 			$loop = new WP_Query( $args );
 			while ( $loop->have_posts() ) : $loop->the_post();
+			    echo '<div class="JL-class1">';
 			    the_title();
-			    echo '<div class="entry-content">';
-			    the_content();
+			    echo '</div>';
+			    echo '<div class="JL-class2">';
+
+			    //the_content();
+			    echo $post->post_content;
+
 			    echo '</div>';
 			endwhile;
 			?>
