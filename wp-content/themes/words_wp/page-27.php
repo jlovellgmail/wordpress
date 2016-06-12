e0a583839fd964c861d3cb18311edfe731539fb6
Jack Lovell <jacklovell@gmail.com>
2016-06-11 18:57:47 -0700

    layout adjustments and make css more specific

--- wp-content/themes/words_wp/page-27.php
+++ wp-content/themes/words_wp/page-27.php
@@ -12,7 +12,8 @@
  * @package words_wp
  */
 
-get_header(); ?>
+get_header(); 
+?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -21,14 +22,13 @@ get_header(); ?>
 			$args = array( 'post_type' => 'movies', 'posts_per_page' => 10 );
 			$loop = new WP_Query( $args );
 			while ( $loop->have_posts() ) : $loop->the_post();
-			    echo '<div class="JL-class1">';
-			    the_title();
-			    echo '</div>';
-			    echo '<div class="JL-class2">';
-
-			    //the_content();
-			    echo $post->post_content;
-
+				echo '<div class="line-item">';
+				    echo '<div class="JL-class1">';
+				    	the_title();
+				    echo '</div>';
+				    echo '<div class="JL-class2">';
+					    echo $post->post_content;
+				    echo '</div>';
 			    echo '</div>';
 			endwhile;
 			?>
